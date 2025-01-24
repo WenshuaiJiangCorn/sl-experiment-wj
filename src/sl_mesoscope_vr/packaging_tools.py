6371955992d05be10f3226334c115cc759567ede
Kushaan Gupta <gkushaan@rediffmail.com>
2025-01-23 21:10:19 -0500

    -- Fixed installation dir -- Added return value after checksum

--- src/sl_mesoscope_vr/packaging_tools.py
+++ src/sl_mesoscope_vr/packaging_tools.py
@@ -46,7 +46,7 @@ def _calculate_file_checksum(base_directory: Path, file_path: Path) -> tuple[str
     return relative_path, checksum.digest()
 
 
-def calculate_directory_checksum(directory: Path, num_processes: int | None = None, batch: bool = False, save_checksum: bool=True) -> None:
+def calculate_directory_checksum(directory: Path, num_processes: int | None = None, batch: bool = False, save_checksum: bool=True) -> str:
     """Calculates xxHash3-128 checksum for the input directory, which includes the data of all contained files and
     the directory structure information.
 
@@ -78,6 +78,9 @@ def calculate_directory_checksum(directory: Path, num_processes: int | None = No
         batch: Determines whether the function is called as part of batch-processing multiple directories. This is used
             to optimize progress reporting to avoid cluttering the terminal.
         save_checksum: Determines whether the checksum should be saved.
+
+    Returns:
+        The xxHash3-128 checksum for the input directory as a hexadecimal string.
     """
     # Determines the number of parallel processes to use.
     if num_processes is None:
@@ -119,8 +122,12 @@ def calculate_directory_checksum(directory: Path, num_processes: int | None = No
             checksum.update(file_path.encode())
             checksum.update(file_checksum)
 
+    checksum_hexstr = checksum.hexdigest()
+
     # Writes the hash to ax_checksum.txt in the root directory
     if save_checksum:
         checksum_path = directory / "ax_checksum.txt"
         with open(checksum_path, "w") as f:
-            f.write(checksum.hexdigest())
+            f.write(checksum_hexstr)
+
+    return checksum_hexstr
