03b6b9ae999cc978d08e38f10667097d94a93c12
WenshuaiJiangCorn <149421832+WenshuaiJiangCorn@users.noreply.github.com>
2025-09-12 16:32:13 -0400

    Create new temp_test file and modify controller port

---
+++ src/yl_experiment/temp_test.py
@@ -0,0 +1,60 @@
+"""This module provides the executable script used to run test experiments with only left valve and lick in the Yapici lab."""
+# WJ: Run this script to start the test
+import time
+from pathlib import Path
+import tempfile
+
+import numpy as np
+import keyboard
+from ataraxis_base_utilities import LogLevel, console
+from ataraxis_data_structures import DataLogger
+
+from yl_experiment.data_processing import process_microcontroller_log
+from microcontroller import AMCInterface
+
+# Note, prevents the context manager from automatically deleting the temporary directory.
+with tempfile.TemporaryDirectory(delete=False) as temp_dir_path:
+    output_dir = Path(temp_dir_path).joinpath("test_output")
+
+
+def run_test() -> None:
+    """Initializes, manages, and terminates a test runtime cycle in the Yapici lab. 
+    Valve deactivated for 5 seconds after dispensing reward."""
+
+    data_logger = DataLogger(output_directory=output_dir, exist_ok=True)
+    mc = AMCInterface(data_logger=data_logger)
+    console.echo(mc._controller._port)
+    try:
+        data_logger.start()  # Has to be done before starting any data-generation processes
+        mc.start()
+        console.echo("Test: started. Press 'q' to quit.", level=LogLevel.SUCCESS)
+
+        while True:
+
+            if keyboard.is_pressed("q"):
+                console.echo("Breaking the test loop due to the 'q' key press.")
+                break
+
+            time.sleep(0.01)
+
+    finally:
+        mc.stop()
+        console.echo("Test: ended.", level=LogLevel.SUCCESS)
+
+        data_logger.stop()  # Data logger needs to be stopped last
+
+        # Combines all log entries into a single .npz log file for each source.
+        data_logger.compress_logs(
+            remove_sources=True, memory_mapping=False, verbose=True, compress=False, verify_integrity=False
+        )
+
+        # Extracts all logged data as module-specific .feather files. These files can be read via
+        # Polars' 'read_ipc' function. Use memory-mapping mode for efficiency.
+        process_microcontroller_log(
+            data_logger=data_logger, microcontroller=mc, output_directory=output_dir.joinpath("processed")
+        )
+
+
+# Run test
+if __name__ == "__main__":
+    run_test()
